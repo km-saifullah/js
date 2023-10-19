c13fcb4fada720eb67f263f8782fdf476070f292
km-saifullah <kmsaifullah16@gmail.com>
2023-10-19 18:13:57 +0600

    comparison operator code added!

---
+++ js/comparisonOperator.js
@@ -0,0 +1,30 @@
+/*
+Comparison Operator
+    <  => Less than
+    >  => Greater than
+    <= => Less than or equal to
+    >= => Greater than or equal to
+    == => Equal to
+    != => Not equal to
+*/
+
+let age = 15;
+age = 22;
+// console.log("Age is greater than 18:", age > 18);
+// console.log("Age is less than 18:", age < 18);
+// console.log("Age is greater than or equal to 18:", age > 18);
+// console.log("Age is less than or equal to 18:", age <= 18);
+
+let count = 100;
+count = 400;
+// console.log("Count is equal to 100:", count == 100);
+// console.log("Count is not equal to 100:", count != 100);
+
+/*
+    Difference between == and ===
+*/
+let number1 = 50;
+let number2 = 50;
+number2 = "50";
+console.log("Number1 and number2 are equal:", number1 == number2);
+console.log("Number1 and number2 are equal:", number1 === number2);
