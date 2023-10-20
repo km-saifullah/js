6fdc9fbd6627408bd7000662808e80db0df91bc4
km-saifullah <kmsaifullah16@gmail.com>
2023-10-20 21:31:27 +0600

    function code added!

---
+++ js/function.js
@@ -0,0 +1,19 @@
+function sayHello() {
+  console.log("Hello World..🌎");
+}
+sayHello(); // function invoke(call)
+
+// Function Parameter and Argument
+function add(a, b) {
+  // here a and b are parameters
+  console.log("a =", a, "b =", b, "a + b =", a + b);
+}
+add(30, 20); // here 30 and 20 are arguments
+
+// Return Statement
+const square = function (x) {
+  return x * x;
+  console.log("Hello Square!");
+};
+console.log(square(10));
+console.log(square(5));
