0cd1eed00dad2e499ca66b86d3525d73df94ec96
km-saifullah <kmsaifullah16@gmail.com>
2023-10-20 21:58:57 +0600

    object code added

---
+++ js/object.js
@@ -0,0 +1,18 @@
+// Object in JS
+let person = {
+  id: 1,
+  firstName: "Khaled",
+  lastName: "Saifullah",
+};
+console.log(person.id);
+console.log(person.firstName);
+console.log(person.lastName);
+
+// Object Destructuring
+let { color, company, price } = {
+  color: "Black",
+  company: "BMW",
+  price: 4000000,
+};
+
+console.log(color, company, price);
